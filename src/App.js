3e3d9f3797e31fdf77bc9a9cec0b9c2b05f2b006
CheezItMan <chris@adadevelopersacademy.org>
2019-12-03 18:41:33 -0800

    import axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import PetList from './components/PetList';
 import PetDetails from './components/PetDetails';
 import SearchBar from './components/SearchBar';
